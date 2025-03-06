36b359ad67cfb97fc0b4b370e17d5dcb421634ac
Shubhi Gupta <shubhisgupta3@gmail.com>
2025-03-06 23:14:06 +0530

    jump game

---
+++ Medium/55. Jump Game.js
@@ -0,0 +1,21 @@
+//55. Jump Game
+
+
+/*
+    Time complexity: O(n)
+    Space complexity: O(1)
+*/
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+    let maxIndex = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > maxIndex) {
+            return false;
+        }
+        maxIndex = Math.max(maxIndex, i + nums[i]);
+    }
+    return true;
+};
